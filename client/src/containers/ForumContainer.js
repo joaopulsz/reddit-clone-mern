0674945405a3c51ef072afc27edef0dbbd197067
joaopulsz <joaopulsz@hotmail.com>
2022-11-28 21:25:26 -0300

    added initial forum page logic

--- client/src/containers/ForumContainer.js
+++ client/src/containers/ForumContainer.js
@@ -1,13 +1,31 @@
-import React from "react";
-// displays a forum and all of its posts
+import React, { useState, useEffect } from "react";
+import Post from '../components/Post'
 
-const ForumContainer = () => {
+const ForumContainer = ({forum}) => {
 
+    const [posts, setPosts] = useState([]);
+
+    const id = forum._id;
+
+    const fetchPosts = async () => {
+        const response = await fetch(`http://localhost:4000/forums/${id}/posts`);
+        const postsData = await response.json();
+        setPosts(postsData);
+    }
+
+    useEffect(() => {
+        fetchPosts();
+    }, [])
+
+    //TODO: fix fetch posts by forum.
+    console.log(posts)
 
     return (
         <div className="forum">
-            <h2>Name of the forum</h2>
-            {/* list of all posts, ordered from newest to oldest */}
+            <h2>{forum.title}</h2>
+            {posts.map((post, index) => {
+                return <Post key={index} post={post}/>
+            })}
         </div>
     )
 }
