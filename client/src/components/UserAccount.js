97e7031da127bb55766132cb5c338c15274046cb
joaopulsz <joaopulsz@hotmail.com>
2022-12-02 08:56:35 -0300

    added logic to fetch and display the user's posts and comments in their account page, as well as initial styling

--- client/src/components/UserAccount.js
+++ client/src/components/UserAccount.js
@@ -3,20 +3,43 @@ import PostBanner from './PostBanner';
 
 const UserAccount = ({ loggedInUser }) => {
 
-    const [user, setUser] = useState({});
+    const [posts, setPosts] = useState([]);
+    const [comments, setComments] = useState([]);
 
-    //TODO:  fetch posts and comments by user and display them
+    const fetchPosts = async () => {
+        const response = await fetch(`http://localhost:4000/users/${loggedInUser._id}/posts`);
+        const userData = await response.json();
+        setPosts(userData.posts);
+    }
+
+    const fetchComments = async () => {
+        const response = await fetch(`http://localhost:4000/users/${loggedInUser._id}/comments`);
+        const userData = await response.json();
+        setComments(userData.comments);
+    }
+
+    useEffect(() => {
+        fetchPosts();
+        fetchComments();
+    }, [])
+
     return (
         <div className="user-account-page">
             <h2>{loggedInUser.username}</h2>
 
-            {/* <div id="user-posts">
-                {user ? user.posts.map((post, index) => {
-            return <PostBanner key={index} post={post} />
-        }) : null}
-            </div> */}
+            <div id="user-data-container">
+                <div id="user-posts">
+                    <h3>Posts</h3>
 
-            <div id="user-comments"></div>
+                    {posts.map((post, index) => {
+                    return <PostBanner key={index} post={post} />})}
+                </div>
+
+                <div id="user-comments">
+                    <h3>Comments</h3>
+
+                </div>
+            </div>
         </div>
     )
 }
